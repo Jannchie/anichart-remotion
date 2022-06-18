d3b3d36133d729a37d3ea38b888a02341e00a230
Jannchie <jannchie@gmail.com>
2022-06-18 16:02:32 +0900

    fix: anichart props
    fix: anichart props

--- src/Anichart.tsx
+++ src/Anichart.tsx
@@ -4,18 +4,20 @@ import { useMemo, useState } from "react";
 import * as ANI from "anichart";
 
 export type AnichartProps = {
-  width: number;
-  height: number;
-  children: React.ReactNode;
-};
-
-export const Anichart: React.FC<{
   initStage: (stage: ANI.Stage) => void;
   left?: number;
   top?: number;
   right?: number;
   bottom?: number;
-}> = ({ initStage, left = 0, top = 0, right = 0, bottom = 0 }) => {
+};
+
+export const Anichart: React.FC<AnichartProps> = ({
+  initStage,
+  left = 0,
+  top = 0,
+  right = 0,
+  bottom = 0,
+}) => {
   const config = useVideoConfig();
   const frame = useCurrentFrame();
   const [handle] = useState(() => delayRender());
