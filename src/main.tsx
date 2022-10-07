f640e4e94e59e125f84d0e347ea71b6bf9e2d2f7
Jannchie <jannchie@gmail.com>
2022-10-07 10:09:01 +0900

    feat: optimize load speed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { continueRender, delayRender } from "remotion";
 import { useCurrentFrame, useVideoConfig } from "remotion";
-import { useMemo, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Stage } from "anichart";
 
 export type AnichartProps = {
@@ -21,35 +21,32 @@ export const Anichart: React.FC<AnichartProps> = ({
   const config = useVideoConfig();
   const frame = useCurrentFrame();
   const [handle] = useState(() => delayRender());
-  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
-  const stage = useMemo(() => {
-    if (canvas !== null) {
-      const s = new Stage(canvas);
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [stage, setStage] = useState<Stage | null>(null);
+  useEffect(() => {
+    if (canvas.current) {
+      const s = new Stage(canvas.current);
       s.options.fps = config.fps;
       s.options.sec = config.durationInFrames / config.fps;
       initStage(s);
-      s.setup();
-      s.loadRecourse().then(() => continueRender(handle));
+      s.loadRecourse().then(() => {
+        s.setup();
+        setStage(s);
+        continueRender(handle)
+      });
       s.render(0);
-      return s;
     }
-  }, [canvas, config.durationInFrames, config.fps, handle, initStage]);
+  }, [canvas.current]);
   if (stage && frame !== stage.frame) {
     stage.frame = frame + 1;
     stage.render();
   }
   return (
     <canvas
-      ref={(canvas) => {
-        setCanvas(canvas);
-      }}
-      width={config.width + 1 - left - right}
-      height={config.height + 1 - top - bottom}
-      style={{
-        position: "absolute",
-        left,
-        top,
-      }}
+      ref={canvas}
+      width={config.width - left - right}
+      height={config.height - top - bottom}
+      style={{ position: "absolute", left, top, }}
     />
   );
 };
